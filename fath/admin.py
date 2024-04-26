0da6d6b52e8e4acd077a919239c9575b455ba1ce
AzizxonAbdulvahobov <azizxonabdulvahobov08@gmail.com>
2024-04-26 17:01:54 +0100

    secon commit

--- fath/admin.py
+++ fath/admin.py
@@ -1,3 +1,41 @@
+# from django.apps import apps
 from django.contrib import admin
-
+from . import models
+from django.utils.safestring import mark_safe
 # Register your models here.
+
+
+# all_models = apps.get_models()
+
+# models_list = []
+
+# for model in all_models:
+#     if not admin.site.is_registered(model):
+#         models_list.append(model)
+
+# for model_class in models_list:
+#     admin.site.register(model_class)
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','name','slug' ,'get_image')
+    prepopulated_fields = {'slug': ('name',)}
+
+    def get_image(self, category):
+        if category.img:
+         return mark_safe(f'<img src="{category.img.url}" width="75px;">')
+        
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name','slug', 'body','price', 'dicount','quantity','category','created','get_image')
+    list_display_links = ('id', 'name')
+
+    def get_image(self, product):
+        if product.img:
+            return mark_safe(f'<img src="{product.img.url}" width="75px;">')
+        
+    prepopulated_fields = {'slug': ('name',)}
+
+
+     
--- fath/models.py
+++ fath/models.py
@@ -5,15 +5,28 @@ from django.db import models
 """Xali tugallanmagan modellar"""
 
 
-class category(models.Model):
-    name = models.TextField(max_length=255, unique=True)
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    img = models.ImageField(upload_to='category_img/')
+    slug = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
-class product(models.Model):
-    category = models.ForeignKey(category, on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='product_img')
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='product_img/')
     name = models.CharField(max_length=255)
     body = models.TextField()
-    price = models.IntegerField()
+    price = models.FloatField()
+    dicount = models.FloatField(blank=True, null=True)
+    quantity = models.FloatField()
+    slug = models.SlugField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    
+
+
